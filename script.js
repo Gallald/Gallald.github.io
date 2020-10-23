e0d8f25807aee86db7b49431456d9a094da07e22
Gallal D <Gallal.Drhn@gmail.com>
2020-10-22 21:28:32 -0400

    menu updates

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //Insert code here to do things
 
+
+
+function openMenu() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  
+  function closeMenu() {
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
